5e8930cb053b14cc85cad4ec580a906fcf55a46b
Arie May Wibowo <arie.wibowo@cimbniaga.co.id>
2021-03-13 14:53:19 +0700

    Initial Commit

---
+++ rnd-main-modular/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  rnd-main-modular
+//
+//  Created by Arie May Wibowo on 13/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
