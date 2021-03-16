d362087941f0381af682f47b93c5f1d720c7605b
Arie May Wibowo <arie.wibowo@cimbniaga.co.id>
2021-03-16 14:05:48 +0700

    push latest rnd project main app

--- rnd-main-modular/MainCoordinator.swift
+++ rnd-main-modular/MainCoordinator.swift
@@ -24,7 +24,7 @@ class MainCoordinator: Coordinator {
     
     func login() {
         let bundle = Bundle(for: LoginViewController.self)
-        let storyBoard : UIStoryboard = UIStoryboard(name: "LoginStoryboard", bundle:bundle)
+        let storyBoard : UIStoryboard = UIStoryboard(name: "LoginStoryboard", bundle:bundle)w
         let nextViewController = storyBoard.instantiateViewController(withIdentifier: "LoginViewController") as! LoginViewController
         nextViewController.modalPresentationStyle = .fullScreen
         navigationController.pushViewController(nextViewController, animated: true)
